e6f2b69a4c3446470dd7face69d471fa5717f8df
ReynerCr <25756546+ReynerCr@users.noreply.github.com>
2021-03-14 14:32:30 -0400

    Modified the fake value for phone number.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,9 +27,9 @@ class UserFactory extends Factory
             'surname' => $this->faker->lastName,
             'id_card' => $this->faker->unique()->numerify('########'),
             'email' => $this->faker->unique()->safeEmail,
-            //'email_verified_at' => now(),
+            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'phone_number' => $this->faker->phoneNumber,
+            'phone_number' => $this->faker->numerify('###########'),
             'address' => $this->faker->sentence(10),
             'remember_token' => Str::random(10),
         ];
