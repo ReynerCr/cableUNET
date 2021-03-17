53f886303acf9174e0b36afd097e401e97042bbd
ReynerCr <25756546+ReynerCr@users.noreply.github.com>
2021-03-16 20:26:39 -0400

    Restored change to invoices table and deleted the migration to add timestamps on suscriptions table.

--- database/migrations/2021_03_11_041804_create_invoices_table.php
+++ database/migrations/2021_03_11_041804_create_invoices_table.php
@@ -16,11 +16,13 @@ class CreateInvoicesTable extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('suscription_id')->unique();
-            $table->timestamps();
 
-            $table->foreign('suscription_id')->
-            references('id')->
-            on('suscriptions');
+            $table->foreign('suscription_id')
+                ->references('id')
+                ->on('suscriptions');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('modified_at')->useCurrent();
         });
     }
 
